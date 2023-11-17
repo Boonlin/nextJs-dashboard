17043e2e233cc41512c6f4befd38efd26933fd84
Boonlin Em <emboolin999@gmail.com>
2023-11-17 10:43:34 +0700

    uu

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,10 +3,11 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
-const links = [
+const data = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'Invoices',
@@ -19,17 +20,17 @@ const links = [
 export default function NavLinks() {
   return (
     <>
-      {links.map((link) => {
-        const LinkIcon = link.icon;
+      {data.map((item) => {
+        const LinkIcon = item.icon;
         return (
-          <a
-            key={link.name}
-            href={link.href}
+          <Link
+            key={item.name}
+            href={item.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </a>
+            <p className="hidden md:block">{item.name}</p>
+          </Link>
         );
       })}
     </>
